4a7c7fb242dd6283de1c823ca8f6a40390f27fd3
IbrahDiarra <ibrahdiarra40@gmail.com>
2024-07-08 00:05:59 +0000

    la fin

--- src/app/website/footer/footer.component.ts
+++ src/app/website/footer/footer.component.ts
@@ -8,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  onHome(){
+    const homeSelection = document.getElementById('home')
+    if(homeSelection){
+      homeSelection.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
+
+  onAbout(){
+    const aboutSelection = document.getElementById('about')
+    if(aboutSelection){
+      aboutSelection.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
+
+  onPopular(){
+    const popularSelection = document.getElementById('popular')
+    if(popularSelection){
+      popularSelection.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
+
+  onMenu(){
+    const menuSelection = document.getElementById('menu')
+    if(menuSelection){
+      menuSelection.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
+
+  onOrder(){
+    const orderSelection = document.getElementById('order')
+    if(orderSelection){
+      orderSelection.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
+
+  onBlogs(){
+    const blogsSelection = document.getElementById('blogs')
+    if(blogsSelection){
+      blogsSelection.scrollIntoView({behavior:'smooth',block:'start'})
+    }
+  }
 
 }
--- src/app/website/site/site.component.ts
+++ src/app/website/site/site.component.ts
@@ -1,11 +1,20 @@
 import { HomeComponent } from '../home/home.component';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CategoryComponent } from '../category/category.component';
+import { AboutComponent } from '../about/about.component';
+import { BannerComponent } from '../banner/banner.component';
+import { MenuComponent } from '../menu/menu.component';
+import { OrderComponent } from '../order/order.component';
+import { BlogsComponent } from '../blogs/blogs.component';
+import { FooterComponent } from '../footer/footer.component';
+import { PopularComponent } from '../popular/popular.component';
 
 @Component({
   selector: 'app-site',
   standalone: true,
-  imports: [HomeComponent,NavbarComponent],
+  imports: [HomeComponent,NavbarComponent,CategoryComponent,AboutComponent,
+    BannerComponent,MenuComponent,OrderComponent,BlogsComponent,FooterComponent,PopularComponent],
   templateUrl: './site.component.html',
   styleUrl: './site.component.scss'
 })
